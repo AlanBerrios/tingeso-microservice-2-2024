5d3e4a62a39efa6930b64e5f4c1b1feb1fec76c9
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-12-08 01:50:49 -0300

    ms readi?

--- ms-client/src/main/java/ms/ms_client/entities/ClientEntity.java
+++ ms-client/src/main/java/ms/ms_client/entities/ClientEntity.java
@@ -16,17 +16,29 @@ public class ClientEntity {
     @Column(unique = true, nullable = false)
     private String rut; // Identificador único de cliente (RUT en vez de ID)
 
+    @Column(nullable = false)
     private String firstName; // Primer nombre del cliente
+    @Column(nullable = false)
     private String lastName; // Apellido del cliente
+    @Column(nullable = false)
     private String email; // Correo electrónico
+    @Column(nullable = false)
     private String password; // Clave de acceso
+    @Column(nullable = false)
     private String phone; // Teléfono del cliente
+    @Column(nullable = false)
     private Double income; // Ingresos del cliente
+    @Column(nullable = false)
     private String creditHistory; // Historial crediticio
+    @Column(nullable = false)
     private Integer age; // Edad del cliente
+    @Column(nullable = false)
     private String employmentType; // Tipo de empleo (Ej. "Empleado", "Independiente")
+    @Column(nullable = false)
     private Integer employmentSeniority; // Antigüedad en el empleo (años)
+    @Column(nullable = false)
     private String historyStatus; // Estado de historial crediticio
+    @Column(nullable = false)
     private Integer pendingDebts; // Cantidad de deudas pendientes
 
 }
